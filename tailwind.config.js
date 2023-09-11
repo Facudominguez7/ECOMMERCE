1ae2473f56be5500ee88bcb42d169733a4fdb2be
Facundo Diaz Dominguez <facudominguez457@gmail.com>
2023-09-11 14:54:02 -0300

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: 'rgb(var(--color-primary)',
+      colornav: 'hsl(var(--color-nav)',
+      colorfooter: '#020617',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
